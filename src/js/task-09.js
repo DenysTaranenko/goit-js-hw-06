92f2d2c2f0bd89d15e8304044cff1ca5aaf8ef3b
DenysTaranenko <deniskin5264@gmail.com>
2022-11-25 09:47:20 +0200

    task 9.05

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector('.body');
+const randomColor = document.querySelector('.color');
+const changeBtn = document.querySelector('.change-color');
+
+
+changeBtn.addEventListener('click', () => {
+  let newColor = getRandomHexColor();
+  body.style.backgroundColor = newColor;
+  randomColor.textContent = newColor;
+})
